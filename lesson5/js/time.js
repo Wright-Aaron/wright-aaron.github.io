ef5f828be3165ddf6f82d704b3bdea0d7479d3e9
Wright-Aaron <wri14006@gmail.com>
2020-02-08 13:29:41 -0600

    Commit

---
+++ lesson5/js/time.js
@@ -0,0 +1,13 @@
+try {
+    document.getElementById('currentYear').innerHTML = new Date().getFullYear();
+}
+catch (e) {
+    alert ["Function not supported"]
+}
+
+try {
+    document.getElementById('lastUpdated').innerHTML = new Date().toLocaleDateString("en-US") + " " + new Date().toLocaleTimeString("en-US");
+}
+catch (e) {
+    alert ["Function not supported"]
+}
